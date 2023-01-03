54f49c3098c35cdbb143b1ef56a0d582a54ff20b
Danilopc <sr_danilo@hotmail.com>
2023-01-03 22:37:38 +0100

    header update menu bars

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 let body = document.body
 let loader = document.getElementById('loader');
-
+const bars = document.querySelector('.bars-menu-icon');
+const barsMenu = document.querySelector('.bars-menu');
 window.addEventListener('load', vanish)
 
 function vanish(){
     loader.classList.add('vanish')
-   
 }
+function openMenu()
+{
+    bars.classList.toggle('open-menu')
+    barsMenu.classList.add('open-bars-menu')
+}
+
